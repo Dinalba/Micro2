348ae1d244faa4d8b480df8406fa9b608a2efc3e
Dinalba Navas <dinalba.navas@correo.unimet.edu.ve>
2021-03-05 19:01:03 -0400

    try2

--- micro2-app/src/app/models/movie.ts
+++ micro2-app/src/app/models/movie.ts
@@ -1,13 +1,14 @@
-export interface Movie{
-  id?: string;
-  titulo: string;
-  poster: string;
-  generos: string;
-  lenguaje: string;
-  presupuesto: string;
-  descripcion: string;
-  popularidad: string;
-  productoras: string;
-  fecha: string;
-  estado: string; 
+export interface Movie {    
+        id?: string;
+        titulo: string;
+        poster: string;
+        generos: string;
+        lenguaje: string;
+        presupuesto: string;
+        descripcion: string;
+        popularidad: string;
+        productoras: string;
+        fecha: string;
+        estado: string; 
+    
 }
